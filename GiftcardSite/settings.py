c6697fb7ea938946a88b973f5075aef697d1260d
home <home@localhost.localdomain>
2021-04-26 23:41:32 +0300

    added virtual enviroment files and modified travis.yml configuration

--- GiftcardSite/settings.py
+++ GiftcardSite/settings.py
@@ -12,16 +12,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import base64
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+env = environ.Env()
+# environ.Env.read_env()
+env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kmgysa#fz+9(z1*=c0ydrjizk*7sthm2ga1z4=^61$cxcq8b$l'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
